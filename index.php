24ee1a421d113f294e818e240018fd7c8b4a5366
prusama <marek.prusa.17@gmail.com>
2017-04-17 21:44:41 +0200

    Added page for creating member

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Chat</title>
+</head>
+<body>
+
+	<header>
+		<h1>Join online chat!</h1>
+	</header>
+
+	<section>
+		<article>
+			<form action="/chat.php">
+				Nickname: <input type="text" name="nick">
+				Color: <input type="color" name="color" value="#4873b9">
+				<input type="submit" value="Start chatting">
+			</form>
+		</article>	
+	</section>
+
+</body>
+</html>
